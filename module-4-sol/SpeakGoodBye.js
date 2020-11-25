0dfc79d86313265dc529287ba4480b53e413cf69
Kamaldeep J <kamaldeep.j@gmail.com>
2020-11-25 22:41:27 +0530

    refactored

--- module-4-sol/SpeakGoodBye.js
+++ module-4-sol/SpeakGoodBye.js
@@ -1,4 +1,4 @@
-speakgoodbye.js 
+(function() {
 // NOTE! The steps in this file are basically identical to the ones you
 // performed in the SpeakHello.js file.
 
@@ -9,23 +9,17 @@ speakgoodbye.js
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-
-(function(){
-	var byeSpeaker = {};
+var byeSpeaker = new Object()
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 var speakWord = "Good Bye";
-byeSpeaker.speak = function (name) {
-  		console.log(speakWord + " " + name);
-	}
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
+byeSpeaker.speak =  console.log(speakWord + " " + name)
 
-window.byeSpeaker = byeSpeaker;
-
-})();
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
-// xxxx.xxxx = byeSpeaker;
+global.byeSpeaker = byeSpeaker;
+})();
